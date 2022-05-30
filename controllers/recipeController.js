83508f3178a28cb3bd48091f8920d8220ecee942
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-05-30 12:10:19 +0600

    added explore categories

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -25,3 +25,31 @@ exports.createCategory = async (req, res) => {
         res.status(500).send({ success: false, message: 'Internal Server Error' });
     }
 };
+
+// explore all categories
+exports.exploreCategories = async (req, res) => {
+    try {
+        const limitNumber = 5;
+        const categories = await Category.find().limit(limitNumber);
+
+        res.render('categories', {
+            title: 'Categories - Recipe Blog',
+            categories,
+        });
+    } catch (error) {
+        res.status(500).send({ success: false, message: 'Internal Server Error' });
+    }
+};
+
+// get single category details
+exports.getSingle = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const category = await Category.findById(id);
+        // res.render('categories', {
+        //     title: 'Category - Recipe Blog',
+        // });
+    } catch (error) {
+        res.status(500).send({ success: false, message: 'Internal Server Error' });
+    }
+};
--- routes/recipe.js
+++ routes/recipe.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // controller
 const recipeController = require('../controllers/recipeController');
 
+// routes
 router.get('/', recipeController.homepage);
+router.get('/home', recipeController.homepage);
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.getSingle);
 
 module.exports = router;
